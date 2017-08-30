f9bbc1817222ecfe06cfd991a66988851c36c0a6
Eduardo <ibredu@gmail.com>
2017-08-30 14:59:58 -0500

    Implemented navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import {HomeComponent} from "./home/home.component";
+import {ContactoComponent} from "./contacto/contacto.component";
+import {RouterModule, Routes} from "@angular/router";
+export const appRoutes:Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'contacto', component: ContactoComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+      HomeComponent,
+      ContactoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
